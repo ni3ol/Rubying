ecc39dda96a70992a826e74f82543a60673604b9
VJCNIC001 <vjcnic001@myuct.ac.za>
2016-06-09 14:53:53 +0200

    More looping

--- loop.rb
+++ loop.rb
@@ -6,3 +6,14 @@ loop do
 	break if name.empty?
 	puts "Hello, " + name + "!"
 end
+
+#How weird is this?
+3.times do
+	puts "I'm dying!"
+end
+
+#And for some more randomness... 
+
+4.times do |count|
+	puts (count+1).to_s + " I'm alive!"
+end
