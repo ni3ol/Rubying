d974f8390ecac87844ea306074f7445cd6f13781
VJCNIC001 <vjcnic001@myuct.ac.za>
2016-06-09 14:14:27 +0200

    puts and gets

--- hello.rb
+++ hello.rb
@@ -1,1 +1,7 @@
-puts "Hello Ruby!"
+#puts print line to terminal
+#gets reads line from terminal
+puts "Hello Human!"
+
+puts "What is your name?"
+name = gets
+puts "Hello, " + name + "! Grrrr...."
